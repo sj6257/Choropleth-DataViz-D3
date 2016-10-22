d2bbea420c1f27af3cfad4bc723cdc4fedd7a4a2
Sandeep Jagtap <sajagtap@iu.edu>
2016-10-22 13:01:45 -0400

    API is working now. See Query data on the console

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
 
 
 // Initialize Variable here.
-const KEY="";
+const KEY="5e63ecf1481c019e28003690bccb25f9f97f5e64";
 const defaultYEAR="2015";
 const defaultRegionType="state";
 var regionName="*";   // represent all unless specified.
@@ -19,7 +19,7 @@ function generateTableCode()
     switch (controlChoice1) {
 
         case "Total Population":
-            tableCode="B0100_3001E";
+            tableCode="B01003_001E";
             break;
 
         case "Age Distribution":
@@ -68,22 +68,24 @@ function createAPI()
 
     var year="2015";
     var regionType="state";
+    generateTableCode()
     var url=baseURL+year+"/acs1?get="+tableCode+"&for="+regionType+":"+regionName+""+"&key="+KEY;
-
+    console.log(url);
   return url;
 }
 
 function requestJSON(url,callback)
 {
-    var data; // a global
 
     d3.json(url, function(error, json) {
         if (error) {
             return console.warn(error);}
         else
         {
-            data = json;
-            callback(null);   // when task is successful
+            console.log("Data: "+json);
+            // when task is successful this call back return values of to await all.
+            // first argument inform error status and second one gives back results.
+            callback(null,json);
         }
 
     });
@@ -103,8 +105,8 @@ function fetchData()
             console.log("Error Occurred while fetching data!");
             throw error;
         }
+        console.log("Gotcha !!");
         console.log(results);
-
         //if there is any pre-processing required we can call that function and user regualr defer there to control flow before drawing map.
         // Start drawing map now
         drawMap();
@@ -122,4 +124,19 @@ function drawDefaultMap(data) {
 
 function drawMap(data) {
     // This function takes data as input and draws map.
+    console.log("Painting map")
 }
+
+function main()
+{
+    controlChoice1="Total Population";
+
+    fetchData();
+
+}
+
+$(document).ready(function() {
+    //here is a good spot to hookup other jQuery listeners
+    main();
+});
+
